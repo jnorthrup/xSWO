d958923f9dbc8e5eed287e4f93642250559c2aa6
Jim Northrup <jim@talkwheel.com>
2011-08-30 16:05:17 -0700

    jim@localhost ~/work/xSWO $ time ./quadbag /vol/big240/uncompressed/yago2.nquads0000000 >/dev/null
    real    0m0.265s
    user    0m0.220s
    sys     0m0.043s

--- quadbag.cpp
+++ quadbag.cpp
@@ -4,9 +4,24 @@ using namespace xModel;
 															 
 int main(int argc,const char**argv){
 
-  quad<>::  normalized_quad_context  qc();
+  quad<pair<long,unsigned char> >:: normalized_quad_context  qc();
   int c=0;
   std::vector<std::string>args(argc);
-  for (int c=0;c<argc;++c)args.push_back(argv[c]);
-  for(std::vector<std::string>::iterator iter=args.begin();iter!=	args.end();++iter)     std::cout << *iter ; 
+  for (int c=0;c<argc;++c)
+    args.push_back(argv[c]);
+
+  for(std::vector<std::string>::iterator iter=args.begin();iter!=args.end();++iter){   
+    std::cout << *iter <<'\n'; 
+    std::ifstream infile((*iter).c_str());
+    while(infile.good()){char buf[1024];
+
+      infile.     getline (buf,1024);
+      string line(buf);
+      cout << line << '\n';
+    }
+    infile.close();
+  }
+  
+  
+
 }
