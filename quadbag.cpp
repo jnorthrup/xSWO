0bd14b11c62e25dba3203f209478b98e06f3edad
Jim Northrup <jim@talkwheel.com>
2011-08-30 13:36:48 -0700

    initial args handler working.

--- quadbag.cpp
+++ quadbag.cpp
@@ -4,6 +4,9 @@ using namespace xModel;
 															 
 int main(int argc,const char**argv){
 
-  quad<>::  normalized_quad_context  c();
- 
+  quad<>::  normalized_quad_context  qc();
+  int c=0;
+  std::vector<std::string>args(argc);
+  for (int c=0;c<argc;++c)args.push_back(argv[c]);
+  for(std::vector<std::string>::iterator iter=args.begin();iter!=	args.end();++iter)     std::cout << *iter ; 
 }
